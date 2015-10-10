c5c55da43795dcabcb63a581cb38317174b8aba6
Steve Kite <steve@actor.im>
2015-10-10 11:14:54 +0300

    Fix download script

--- Scripts/download.sh
+++ Scripts/download.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 set -ev
 
-sdk_version=0.0.1
+sdk_version=0.0.2
 sha1_checksum=cec339b322fabacfb7857ce740c64059bb83d46d
 
 rm -fr Frameworks
 
 echo "fetching ActorSDK.framework"
-curl -OL https://github.com/actorapp/J2ObjC-Framework/releases/download/v${sdk_version}/ActorSDK.framework.zip
+curl -OL https://github.com/actorapp/ActorSDK-iOS/releases/download/v${sdk_version}/ActorSDK.framework.zip
 echo "${sha1_checksum}  ActorSDK.framework.zip" | shasum -c
 unzip -o -q ActorSDK.framework.zip
 mkdir -p Frameworks
