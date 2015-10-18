090aa5795c519d26d40a3e7d60286cc4959fd0cc
Steve Kite <steve@actor.im>
2015-10-18 07:02:26 +0300

    Update download scripts

--- Scripts/download.sh
+++ Scripts/download.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 set -ev
 
-sdk_version=$(cat SDK_VER)
-sha1_checksum=$(cat SDK_SHA)
+sdk_version=$(cat Scripts/SDK_VER)
+sha1_checksum=$(cat Scripts/SDK_SHA)
 
 rm -fr Frameworks
 
 echo "fetching ActorSDK.framework"
-curl -OL https://github.com/actorapp/ActorSDK-iOS/releases/download/v${sdk_version}/ActorSDK.framework.zip
-echo "${sha1_checksum}  ActorSDK.framework.zip" | shasum -c
-unzip -o -q ActorSDK.framework.zip
+curl -OL https://github.com/actorapp/ActorSDK-iOS/releases/download/v${sdk_version}/ActorSDK.zip
+echo "${sha1_checksum}  ActorSDK.zip" | shasum -c
+unzip -o -q ActorSDK.zip
 mkdir -p Frameworks
 mv ActorSDK.framework Frameworks
-rm ActorSDK.framework.zip
+mv ActorSDK.framework.dSYM Frameworks
+rm ActorSDK.zip
