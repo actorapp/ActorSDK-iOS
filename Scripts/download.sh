1beb1449df4997271f95627350d48ba26cab582a
Steve Kite <steve@actor.im>
2015-12-06 17:52:37 +0300

    Update download.sh

--- Scripts/download.sh
+++ Scripts/download.sh
@@ -8,7 +8,11 @@ rm -fr Frameworks
 
 echo "fetching ActorSDK.framework"
 curl -OL https://github.com/actorapp/ActorSDK-iOS/releases/download/v${sdk_version}/ActorSDK.zip
+
 echo "${sha1_checksum}  ActorSDK.zip" | shasum -c
+echo "Original: ${sha1_checksum}"
+shasum "Local: ActorSDK.zip"
+
 unzip -o -q ActorSDK.zip
 mkdir -p Frameworks
 mv ActorSDK.framework Frameworks
